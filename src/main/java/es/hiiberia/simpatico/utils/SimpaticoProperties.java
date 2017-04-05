6ab4e146720fa8cafe2f6b7c8aa771ba9ce31dd6
kazhamiakin <kazhamiakin@gmail.com>
2017-04-05 14:04:03 +0200

    resource files

--- src/main/java/es/hiiberia/simpatico/utils/SimpaticoProperties.java
+++ src/main/java/es/hiiberia/simpatico/utils/SimpaticoProperties.java
@@ -1,7 +1,6 @@
 package es.hiiberia.simpatico.utils;
 
 import java.util.MissingResourceException;
-import java.util.Properties;
 import java.util.ResourceBundle;
 
 import org.apache.log4j.Logger;
@@ -36,29 +35,28 @@ public class SimpaticoProperties {
 	public static boolean getStrings() {
 		boolean result = false;
 		
-		Properties RESOURCE_BUNDLE = new Properties();
+		ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle("simpatico");
 		
 		try {			
-			RESOURCE_BUNDLE.load(SimpaticoProperties.class.getResourceAsStream("/simpatico.properties"));
 			// Database
-			elasticSearchIp = RESOURCE_BUNDLE.getProperty("elasticsearch.ip");
-			elasticSearchPort = Integer.parseInt(RESOURCE_BUNDLE.getProperty("elasticsearch.port"));
-			elasticSearchClusterName = RESOURCE_BUNDLE.getProperty("elasticsearch.clustername");
+			elasticSearchIp = RESOURCE_BUNDLE.getString("elasticsearch.ip");
+			elasticSearchPort = Integer.parseInt(RESOURCE_BUNDLE.getString("elasticsearch.port"));
+			elasticSearchClusterName = RESOURCE_BUNDLE.getString("elasticsearch.clustername");
 			
-			elasticSearchHIIndex = RESOURCE_BUNDLE.getProperty("elasticsearch.hi.index");
-			elasticSearchSharedIndex = RESOURCE_BUNDLE.getProperty("elasticsearch.shared.index");
+			elasticSearchHIIndex = RESOURCE_BUNDLE.getString("elasticsearch.hi.index");
+			elasticSearchSharedIndex = RESOURCE_BUNDLE.getString("elasticsearch.shared.index");
 			
-			elasticSearchCreatedFieldName = RESOURCE_BUNDLE.getProperty("elasticsearch.created.field.name");
-			elasticSearchFieldSearch = RESOURCE_BUNDLE.getProperty("elasticsearch.search.field");
+			elasticSearchCreatedFieldName = RESOURCE_BUNDLE.getString("elasticsearch.created.field.name");
+			elasticSearchFieldSearch = RESOURCE_BUNDLE.getString("elasticsearch.search.field");
 			
 			// Piwik
-			piwikApiUrl = RESOURCE_BUNDLE.getProperty("piwik.api_url");
-			piwikAuthToken = RESOURCE_BUNDLE.getProperty("piwik.auth_token");
-			elasticSearchPiwikIndex = RESOURCE_BUNDLE.getProperty("elasticsearch.piwik.index");
-			elasticSearchPiwikType = RESOURCE_BUNDLE.getProperty("elasticsearch.piwik.type");
+			piwikApiUrl = RESOURCE_BUNDLE.getString("piwik.api_url");
+			piwikAuthToken = RESOURCE_BUNDLE.getString("piwik.auth_token");
+			elasticSearchPiwikIndex = RESOURCE_BUNDLE.getString("elasticsearch.piwik.index");
+			elasticSearchPiwikType = RESOURCE_BUNDLE.getString("elasticsearch.piwik.type");
 			
 			result = true;
-		} catch (Exception e) {
+		} catch (MissingResourceException e) {
 			e.printStackTrace();
 			Logger.getRootLogger().error("Properties file error");
 		}
